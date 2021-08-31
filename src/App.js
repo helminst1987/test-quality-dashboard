5f9ea9d69bdbcf6ac34e4aef613a3908db09cbf6
unknown <helminstlive@gmail.com>
2021-08-31 07:59:44 +0700

    Create comming soon page

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'roboto'
+    }}>
+      <h1 style={{ 
+        color: '#6EC177',
+        margin: 0
+      }}>Quality Dashboard</h1>
+    </div>
+  )
+}
+
+export default App
